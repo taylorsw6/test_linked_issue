e29d48d34018e01498d330d55cba7c949235154e
Ramon Mulia <ramonmulia@gmail.com>
2022-01-12 09:30:20 -0300

    PR_

--- src/action.js
+++ src/action.js
@@ -7,7 +7,7 @@ const format = (obj) => JSON.stringify(obj, undefined, 2);
 
 async function run() {
   core.info(`
-    *** ACTION RUN - START ***
+    *** ACTION RUN - START  ***
     `);
 
   try {
